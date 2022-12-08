f264dc67b695c86181fce05fb78cc2a36e6d46a5
alecejones-noodoe <alecejones@noodoe.com>
2022-12-08 13:20:28 +0800

    fix: add server blueprints volume

--- src/getDockerCompose.ts
+++ src/getDockerCompose.ts
@@ -20,7 +20,7 @@ const getDockerCompose = (config) => {
   const dockerCompose = `# docker-compose.yml
 version: "3.8"
 
-networks: 
+networks:
     internal-network:
         driver: bridge
         name: wprt_public
@@ -37,7 +37,7 @@ services:
             - server_content:/var/www/wordparrot/server/content:ro
             - server_plugins:/var/www/wordparrot/server/plugins:ro
             - authorized_domains:/var/www/wordparrot/authorized_domains:ro
-        networks: 
+        networks:
             - internal-network
         env_file:
             - .env
@@ -51,7 +51,7 @@ services:
     sites_server:
         image: alecejones/wordparrot:${config.sitesVersion}
         restart: always
-        env_file: 
+        env_file:
             - .env
         ports:
             - 5000:5000
@@ -62,20 +62,21 @@ services:
             - server_content:/var/www/wordparrot/server/content:rw
             - server_bull:/var/www/wordparrot/server/bull:rw
             - server_plugins:/var/www/wordparrot/server/plugins:rw
+            - server_blueprints:/var/www/wordparrot/server/blueprints:rw
             - authorized_domains:/var/www/wordparrot/server/authorized_domains:rw
-        networks: 
+        networks:
             - internal-network
 
     sandbox_server:
         image: alecejones/wordparrot-sandbox:${config.sandboxVersion}
         restart: always
-        env_file: 
+        env_file:
             - .env
         volumes:
             - redis_data:/data
         ports:
             - 6060:6060
-        networks: 
+        networks:
             - internal-network
         
     ${config.dbHost}:
@@ -93,13 +94,13 @@ services:
             - .env
         ports:
             - 3306:3306
-        networks: 
+        networks:
             - internal-network
 
     phpmyadmin:
         image: phpmyadmin
         restart: always
-        env_file: 
+        env_file:
             - .env
         environment:
             PMA_HOST: ${config.dbHost}
@@ -110,20 +111,20 @@ services:
             - 8080:80
         depends_on:
             - ${config.dbHost}
-        networks: 
+        networks:
             - internal-network
 
     ${config.redisServerName}:
         image: redis:alpine
         restart: always
-        env_file: 
+        env_file:
             - .env
         command: redis-server --requirepass \${REDIS_PASSWORD\}
         volumes:
             - redis_data:/data
         ports:
             - 6379:6379
-        networks: 
+        networks:
             - internal-network
 
 volumes:
@@ -151,6 +152,9 @@ volumes:
     server_plugins:
         driver: local
         name: server_plugins
+    server_blueprints:
+        driver: local
+        name: server_blueprints
     server_bull:
         driver: local
         name: server_bull
